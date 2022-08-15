a5e425d24e766b34a076620d3a23be70a87c856a
Matheus Martinez <matheus.martinez@ctw.bmwgroup.com>
2022-08-15 21:04:56 +0100

    Remove comented code

--- src/utils/firebase-client.ts
+++ src/utils/firebase-client.ts
@@ -8,12 +8,4 @@ export const FirebaseClient = initializeApp({
   messagingSenderId: import.meta.env.VITE_MESSAGE_SENDER_ID,
   appId: import.meta.env.VITE_APP_ID,
   measurementId: import.meta.env.VITE_MEASUREMENT_ID,
-
-  // apiKey: "AIzaSyA_gHHblzeVzsqruHiw9ZjcjLjp2SeLolo",
-  // authDomain: "react-firebase-chat-4f595.firebaseapp.com",
-  // projectId: "react-firebase-chat-4f595",
-  // storageBucket: "react-firebase-chat-4f595.appspot.com",
-  // messagingSenderId: "895303272197",
-  // appId: "1:895303272197:web:5a82dbcc84f085ca91996c",
-  // measurementId: "G-JM1J9559CZ",
 });
